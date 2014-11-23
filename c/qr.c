8e1cdd452e5553a6adc22333b2621e8bce834d53
Thomas ten Cate <ttencate@gmail.com>
2014-11-23 15:11:39 +0100

    Add alignment patterns

--- c/qr.c
+++ c/qr.c
@@ -5,6 +5,8 @@
 #define DARK 0x00
 #define LIGHT 0xff
 
+const int alignment_patterns[] = {6, 30, 58, 86, 114, 142, 170};
+
 bool is_function_pattern(int i, int j) {
   // Finder patterns
   if (i < 8 && j < 8) {
@@ -15,6 +17,19 @@ bool is_function_pattern(int i, int j) {
   }
   if (i < 8 && j >= MODULES_PER_SIDE - 8) {
     return true;
+  }
+  // Alignment patterns
+  for (int m = 0; m < 7; m++) {
+    if (i - alignment_patterns[m] >= -2 && i - alignment_patterns[m] <= 2) {
+      for (int n = 0; n < 7; n++) {
+        if (m == 0 && n == 0) continue;
+        if (m == 0 && n == 6) continue;
+        if (m == 6 && n == 0) continue;
+        if (j - alignment_patterns[n] >= -2 && j - alignment_patterns[n] <= 2) {
+          return true;
+        }
+      }
+    }
   }
   // Timing patterns
   if (i == 6 || j == 6) {
@@ -36,6 +51,16 @@ unsigned char finder_pattern_at(int i, int j) {
   return DARK;
 }
 
+unsigned char alignment_pattern_at(int i, int j) {
+  if (i == -2 || i == 2 || j == -2 || j == 2) {
+    return DARK;
+  }
+  if (i == 0 && j == 0) {
+    return DARK;
+  }
+  return LIGHT;
+}
+
 unsigned char function_pattern_at(int i, int j) {
   // Finder patterns
   if (i <= 7 && j <= 7) {
@@ -47,11 +72,18 @@ unsigned char function_pattern_at(int i, int j) {
   if (i <= 7 && j >= MODULES_PER_SIDE - 8) {
     return finder_pattern_at(i, j - MODULES_PER_SIDE + 7);
   }
-  // Timing patterns
-  if (i == 6 || j == 6) {
-    return ((i+j) % 2) ? LIGHT : DARK;
+  // Alignment patterns
+  for (int m = 0; m < sizeof(alignment_patterns) / sizeof(int); m++) {
+    if (i - alignment_patterns[m] >= -2 && i - alignment_patterns[m] <= 2) {
+      for (int n = 0; n < sizeof(alignment_patterns) / sizeof(int); n++) {
+        if (j - alignment_patterns[n] >= -2 && j - alignment_patterns[n] <= 2) {
+          return alignment_pattern_at(i - alignment_patterns[m], j - alignment_patterns[n]);
+        }
+      }
+    }
   }
-  return DARK;
+  // Timing patterns
+  return ((i+j) % 2) ? LIGHT : DARK;
 }
 
 void qr(const char *input, int count, unsigned char *output) {
@@ -61,5 +93,4 @@ void qr(const char *input, int count, unsigned char *output) {
         is_function_pattern(i, j) ? function_pattern_at(i, j) : LIGHT;
     }
   }
-  return;
 }
